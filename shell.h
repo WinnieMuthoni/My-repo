ff16a066266ced7c958f7ec65a487140eb07df76
Josephwawer <josephgichohi322@gmail.com>
2023-04-22 08:27:29 -0700

    Shell.h

--- shell.h
+++ shell.h
@@ -11,11 +11,12 @@
 #include <syst/stat.h>
 #include <limits.h>
 #include <sys/wait>
+#include <sys/types.h>
 
 
-#define READ_BUF_SIZE 1024
-#define WRITE_BUF_SIZE 1024
-#define BUF_FLUSH -1
+#define WRITE_BUFF_SIZE 1024
+#define READ_BUFF_SIZE 1024
+#define BUFf_FLUSH -1
 #define CMD_AND 
 #define CMD_OR 
 #define CMD_NORM 
@@ -23,70 +24,73 @@
 
 #define CONVERT_LOWERCASE 1
 #define CONVERT_UNSIGNED 2
-#define USE_GETLINE 0
-#define USE_STRTOK
+#define USE_GETLINE 0 
+#define USE_STRTOK 0
 #define HISTO_MAX 4096
-#define HISTO_FILE ".simple_shell_history"
+#define HISTO_FILE ".shell_history"
 
 extern char **environ;
 
 /**
- * struct listst - singly linked list
- * @numbe; the number field
- * @st: a string
+ * struct list_st - singly linked list
+ * @ne; the number field
+ * @stng: a string
  * @z: points to the next node
  *
  */
 
-typedef struct listst
+typedef struct list_st
 {
-	int numbe;
-	char *st;
+	int ne;
+	char *stng;
 	struct listst *next;
-}listt
+}lines;
+
 
 /**
  *
- * struct pas - contains pseudocode-arguments to pass into a funtion
+ * struct pas - contains pseudocode - arguments to pass into a funtion
  * @arg: a string generated from the getline containing arguments
  * * @argv:an array of strings generated from arg
  * @path: a string path for the current command
  * @argc: the argument count
- * @_count: the error count
- * @er_n: the error code for exit()s
- * @count_flag: if on count this line of input
+ * @error_count: the error count
+ * @status: the return status of the last exec'd command
+ * @cmd_buff: address of pointer to cmd_buf, on if chaining
+ * @cmd_buff_type: CMD_type ||, &&, ;
+ * @readfd: the fd from which to read line input
+ * @error_Num: the error code for exit()s
+ * @count_flag: if it count this line of input
  * @fname: the program filename
- * @env: linked list local copy of environ
+ * @envi: linked list local copy of environ
  * @envm: custom modified copy of environ from LL env
  * @history: the history node
  * @alias: the alias node
- * @env_h: on if environ was changed
- * @status: the return status of the last exec'd command
- * @cmd_buf: address of pointer to cmd_buf, on if chaining
- * @cmd_buf_type: CMD_type ||, &&, ;
- * @readfd: the fd from which to read line input
+ * @env_h:  if environ was changed
  * @tcount: the history line number count
  */
+
+
 typedef struct pas
 {
 	char *arg;
 	char **argv;
 	char *path;
 	int argc;
-	unsigned int count ;
-	int er_n;
+	unsigned int error_count;
+	int status;
+	char **cmd_buff;
+        int cmd_buff_type;
+        int readfd;
+	int error_Num;
 	int count_flag;
 	char *fname;
-	listt *env;
-	listt *history;
-	listt *alias;
+	lines *env;
+	lines *history;
+	lines *alias;
 	char **envm;
 	int env_h;
 	int status;
-
-	char **cmd_buf;
-	int cmd_buf_type;
-	int readfd;
 	int tcount;
 }forn
 
@@ -95,15 +99,19 @@ typedef struct pas
 		0, 0, 0}
 
 /**
- * struct builtin - contains a builtin string and related function
+ * struct bu_in - contains a builtin string and related function
  * @type: the builtin command flag
  * @func: the function
  */
-typedef struct builtin
+typedef struct bu_in
 {
 	char *type;
 	int (*func)(forn *);
 } in_ta
+
+int _exit(forn *);
+int _sh(forn *);
+int _helper(forn *);
 
 int hsh(forn *, char **);
 int d_build(forn *);
@@ -119,7 +127,7 @@ int loophsh(char **);
 void _eputs(char *);
 int _eputchar(char);
 int _putfd(char c, int fd);
-int _putsfd(char *str, int fd);
+int _putsfd(char *stng, int fd);
 int _strlen(char *);
 int _strcmp(char *, char *);
 char *starts_with(const char *, const char *);
@@ -146,9 +154,6 @@ void print_error(forn *, char *);
 int print_d(int, int);
 char *convert_number(long int, int, int);
 void remove_comments(char *);
-int _myexit(forn *);
-int _mycd(forn *);
-int _myhelp(forn *);
 int _myhistory(forn *);
 int _myalias(forn *);
 ssize_t get_input(forn *);
@@ -171,24 +176,24 @@ int populate_env_list(forn *);
 char **get_environ(forn *);
 int _unsetenv(forn *, char *);
 int _setenv(forn *, char *, char *);
-char *get_history_file(forn *info);
-int write_history(forn *info);
-int read_history(forn *info);
-int build_history_list(forn *info, char *buf, int linecount);
-int renumber_history(forn *info);
-listt *add_node(listt **, const char *, int);
-listt *add_node_end(listt **, const char *, int);
-size_t print_list_str(const listt *);
-int delete_node_at_index(listt **, unsigned int);
-void free_list(listt **);
+char *get_history_file(forn *fort);
+int write_history(forn *fort);
+int read_history(forn *fort);
+int build_history_list(forn *fort, char *buff, int linecount);
+int renumber_history(forn *fort);
+lines *add_node(lines **, const char *, int);
+lines *add_node_end(lines **, const char *, int);
+size_t print_list_str(const lines *);
+int delete_node_at_index(lines **, unsigned int);
+void free_list(lines **);
 
 
 
-size_t list_len(const listt *);
-char **list_to_strings(listt *);
-size_t print_list(const listt *);
-list_t *node_starts_with(listt *, char *, char);
-ssize_t get_node_index(listt *, listt *);
+size_t list_len(const lines *);
+char **list_to_strings(lines *);
+size_t print_list(const lines *);
+lines *node_starts_with(lines *, char *, char);
+ssize_t get_node_index(lines *, lines *);
 
 
 
