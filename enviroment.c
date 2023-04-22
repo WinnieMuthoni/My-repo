0714adf9c22da1ba86c71310ee1691fa3195690f
2003gichohiwaweru <josephgichohi322>
2023-04-22 21:09:08 +0300

    Environment.c

--- enviroment.c
+++ enviroment.c
@@ -1,5 +1,22 @@
 #include "shell.h"
 
+
+
+/**
+ * _uEnvim - prints the current Environment
+ * @fort: Structure that contains arguments.
+ * 	  Used to maintain the constants prototype functions
+ * Return: 0 Always
+ *
+ */
+int _uEnvim(forn *fort)
+{
+	print_list_st(fort ->env;);
+	return (0);
+}
+
+
+
 /**
  * setEnv - Initialize a new environment variable 
  * 	    or modify an existing one
@@ -20,6 +37,28 @@ int _setEnv(forn *fort)
 		return (0);
 	return (1);
 }
+
+
+/**
+ * envPop - Populates the linked list of the environment
+ *
+ * @fort: Struture that containts  the arguments.
+ * 	  Used to maintain the constant prototype function.
+ * Return: 0 Always
+ *
+ */
+int envPop(forn *fort)
+{
+	lines *nod = NULL;
+	size_t y;
+
+	for (y = 0; envm[y]; y++)
+		add_nod_end(&nod, envm[y], 0);
+	fort -> env = nod;
+	return (0);
+}
+
+
 
 /**
  * unSetEnv - Removes an environment variable
