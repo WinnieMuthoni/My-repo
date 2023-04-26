569808efdf18a1e897658127bf6ed187a4ef8e3d
Josephwawer <josephgichohi322@gmail.com>
2023-04-26 07:36:20 -0700

    build_in.c

--- build_in.c
+++ build_in.c
@@ -3,80 +3,77 @@
 
 
 /**
- * _helper - changes the current directory of the process
- * @fort: Contains potential arguments
- *        Maintains onstants prototype functions.
+ * _help - changes the current directory of the process
+ * @fort: Contains potential arguments. Maintains constants prototype functions
  * Return: 0 Always
  *
  */
 
-int _myhelp(forn *fort)
+int _help(forn *fort)
 {
-        char **ary;
+	char **ary;
 
-        ary = fort -> argv;
-        _puts("Helper, \n");
-        if (0)
-                _puts(*ary);
-        return (0)
+	ary = fort->argv;
+	puts("Helper, \n");
+	if (0)
+		_puts(*ary);
+	return (0);
 }
 
 
 /**
- * _exit - exits the shell
- * @fort: Contains potential arguments
- * 	  Used to maintain prototype function
+ * _mexit - exits the shell
+ * @fort: Contains potential arguments. Used to maintain prototype function
  * Return: Exits shell with a given status
- * 	   (0) if fort.argv[0] = "Exit"
+ * 	(0) if fort.argv[0] = "Exit"
  */
 
-int _myexit(forn *fort)
+int _mexit(forn *fort)
 {
 	int exit_ch;
 
-	if (fort -> argv[1])
+	if (fort->argv[1])
 	{
-		exit_ch = _error_atoi(fort -> argv[1]);
+		exit_ch = _erratoi(fort->argv[1]);
 		if (exit_ch == -1)
 		{
-			info -> status = 2;
+			fort->status = 2;
 			print_error(fort, "Illegal: ");
-			_eputs(fort -> argv[1]);
+			_eputs(fort->argv[1]);
 			_eputchar('\n');
 			return (1);
 		}
-	fort -> error_Num = _error_atoi(fort -> argv[1]);
+	fort->error_Num = _erratoi(fort->argv[1]);
 	return (-2);
 	}
-	fort -> error_Num = -1;
+	fort->error_Num = -1;
 	return (-2);
 }
 
 
 /**
- * _mycd - changes the  current directory of the process
- * @fort: Contains potential arguments
- * 	  Used to maintain prototype functions
+ * _cd - changes the  current directory of the process
+ * @fort: Contains potential arguments. Used to maintain prototype functions
  * Return: 0 always
  */
 
-int _mycd(forn *fort)
+int _cd(forn *fort)
 {
 	char *n, *dire, buff[1024];
 	int dir_ch;
 
-	n = cget(buff, 1024);
+	n = getcwd(buff, 1024);
 	if (!n)
 		_puts("Failure: \n");
-	if (!fort -> argv[1])
+	if (!fort->argv[1])
 	{
 		dire = _getenv(fort, "HOME is = ");
-		if (!dere)
-			dir_ch = chdir((dere = _getenv(fort, "PWD = ")) ? dere : "/");
+		if (!dire)
+			dir_ch = chdir((dire = _getenv(fort, "PWD = ")) ? dire : "/");
 		else
-			dir_ch = dir_ch(dere);
+			dir_ch = chdir(dire);
 	}
-	else if (_strcmp(fort -> argv[1], "-") == 0)
+	else if (_strcmp(fort->argv[1], "-") == 0)
 	{
 		if (!_getenv(fort, "PWDOLD = "))
 		{
@@ -85,20 +82,20 @@ int _mycd(forn *fort)
 			return (1);
 		}
 		_puts(_getenv(fort, "PWDOLD = ")), _putchar('\n');
-		dir_ch = chdir(( dere = _getenv(fort, "PWDOLD = ")) ? dere : "/";
+		dir_ch = chdir((dire = _getenv(fort, "PWDOLD = ")) ? dire : "/");
 	}
 	else
-		dir_ch = chdir(fort -> argv[1]);
+		dir_ch = chdir(fort->argv[1]);
 
 	if (dir_ch == -1)
 	{
 		print_error(fort, ": ");
-		_eputs(fort -> argv[1]), _eputchar('\n');
+		_eputs(fort->argv[1]), _eputchar('\n');
 	}
 	else
 	{
-		_setenvir(fort, "PWDOLD", _getenv(fort, "PWD = "));
-		_setenvir(fort, "PWD", getcwd(buff, 1024));
+		_setenv(fort, "PWDOLD", _getenv(fort, "PWD = "));
+		_setenv(fort, "PWD", getcwd(buff, 1024));
 	}
 	return (0);
 }
